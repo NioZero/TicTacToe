07be4dd7c1d42ba95b8d63be9871b65b76048126
NioZero <ghidalgo@frostdisk.com>
2019-04-04 13:39:20 -0300

    Day1: Drawing Sprites and the Board

--- Classes/HelloWorldScene.cpp
+++ Classes/HelloWorldScene.cpp
@@ -1,18 +1,18 @@
 /****************************************************************************
  Copyright (c) 2017-2018 Xiamen Yaji Software Co., Ltd.
- 
+
  http://www.cocos2d-x.org
- 
+
  Permission is hereby granted, free of charge, to any person obtaining a copy
  of this software and associated documentation files (the "Software"), to deal
  in the Software without restriction, including without limitation the rights
  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  copies of the Software, and to permit persons to whom the Software is
  furnished to do so, subject to the following conditions:
- 
+
  The above copyright notice and this permission notice shall be included in
  all copies or substantial portions of the Software.
- 
+
  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -29,105 +29,103 @@ USING_NS_CC;
 
 Scene* HelloWorld::createScene()
 {
-    return HelloWorld::create();
+	return HelloWorld::create();
 }
 
 // Print useful error message instead of segfaulting when files are not there.
 static void problemLoading(const char* filename)
 {
-    printf("Error while loading: %s\n", filename);
-    printf("Depending on how you compiled you might have to add 'Resources/' in front of filenames in HelloWorldScene.cpp\n");
+	printf("Error while loading: %s\n", filename);
+	printf("Depending on how you compiled you might have to add 'Resources/' in front of filenames in HelloWorldScene.cpp\n");
 }
 
 // on "init" you need to initialize your instance
 bool HelloWorld::init()
 {
-    //////////////////////////////
-    // 1. super init first
-    if ( !Scene::init() )
-    {
-        return false;
-    }
+	if (!Scene::init())
+	{
+		return false;
+	}
 
-    auto visibleSize = Director::getInstance()->getVisibleSize();
-    Vec2 origin = Director::getInstance()->getVisibleOrigin();
+	_turn = 1;
 
-    /////////////////////////////
-    // 2. add a menu item with "X" image, which is clicked to quit the program
-    //    you may modify it.
+	auto visibleSize = Director::getInstance()->getVisibleSize();
+	Vec2 origin = Director::getInstance()->getVisibleOrigin();
 
-    // add a "close" icon to exit the progress. it's an autorelease object
-    auto closeItem = MenuItemImage::create(
-                                           "CloseNormal.png",
-                                           "CloseSelected.png",
-                                           CC_CALLBACK_1(HelloWorld::menuCloseCallback, this));
+	_originX = visibleSize.width / 2 - (_spriteSize * 1.5 + _spriteMargin * 3);
+	_originY = visibleSize.height / 2 - (_spriteSize * 1.5 + _spriteMargin * 3);
 
-    if (closeItem == nullptr ||
-        closeItem->getContentSize().width <= 0 ||
-        closeItem->getContentSize().height <= 0)
-    {
-        problemLoading("'CloseNormal.png' and 'CloseSelected.png'");
-    }
-    else
-    {
-        float x = origin.x + visibleSize.width - closeItem->getContentSize().width/2;
-        float y = origin.y + closeItem->getContentSize().height/2;
-        closeItem->setPosition(Vec2(x,y));
-    }
+	auto _bgColor = LayerColor::create(Color4B(0xFF, 0xFF, 0xFF, 0xFF));
+	this->addChild(_bgColor, -10);
 
-    // create menu, it's an autorelease object
-    auto menu = Menu::create(closeItem, NULL);
-    menu->setPosition(Vec2::ZERO);
-    this->addChild(menu, 1);
+	#ifdef _DEBUG
+	auto square = DrawNode::create();
+	square->drawRect(Point(_originX, _originY), Point(_originX + _spriteSize * 3 + _spriteMargin * 6, _originY + _spriteSize * 3 + _spriteMargin * 6), Color4F::BLUE);
+	this->addChild(square);
+	#endif
 
-    /////////////////////////////
-    // 3. add your codes below...
+	auto verticalLine1 = DrawNode::create();
+	verticalLine1->drawLine(Point(_originX + _spriteSize + _spriteMargin * 2, _originY), Point(_originX + _spriteSize + _spriteMargin * 2, _originY + _spriteMargin * 6 + _spriteSize * 3), Color4F::RED);
+	verticalLine1->setLineWidth(4.0f);
+	this->addChild(verticalLine1);
 
-    // add a label shows "Hello World"
-    // create and initialize a label
+	auto verticalLine2 = DrawNode::create();
+	verticalLine2->drawLine(Point(_originX + _spriteSize * 2 + _spriteMargin * 4, _originY), Point(_originX + _spriteSize * 2 + _spriteMargin * 4, _originY + _spriteMargin * 6 + _spriteSize * 3), Color4F::RED);
+	verticalLine2->setLineWidth(4.0f);
+	this->addChild(verticalLine2);
 
-    auto label = Label::createWithTTF("Hello World", "fonts/Marker Felt.ttf", 24);
-    if (label == nullptr)
-    {
-        problemLoading("'fonts/Marker Felt.ttf'");
-    }
-    else
-    {
-        // position the label on the center of the screen
-        label->setPosition(Vec2(origin.x + visibleSize.width/2,
-                                origin.y + visibleSize.height - label->getContentSize().height));
+	auto horizontalLine1 = DrawNode::create();
+	horizontalLine1->drawLine(Point(_originX, _originY + _spriteSize + _spriteMargin * 2), Point(_originX + _spriteMargin * 6 + _spriteSize * 3, _originY + _spriteSize + _spriteMargin * 2), Color4F::RED);
+	horizontalLine1->setLineWidth(4.0f);
+	this->addChild(horizontalLine1);
 
-        // add the label as a child to this layer
-        this->addChild(label, 1);
-    }
+	auto horizontalLine2 = DrawNode::create();
+	horizontalLine2->drawLine(Point(_originX, _originY + _spriteSize * 2 + _spriteMargin * 4), Point(_originX + _spriteMargin * 6 + _spriteSize * 3, _originY + _spriteSize * 2 + _spriteMargin * 4), Color4F::RED);
+	horizontalLine2->setLineWidth(4.0f);
+	this->addChild(horizontalLine2);
 
-    // add "HelloWorld" splash screen"
-    auto sprite = Sprite::create("HelloWorld.png");
-    if (sprite == nullptr)
-    {
-        problemLoading("'HelloWorld.png'");
-    }
-    else
-    {
-        // position the sprite on the center of the screen
-        sprite->setPosition(Vec2(visibleSize.width/2 + origin.x, visibleSize.height/2 + origin.y));
+	auto touchListener = EventListenerTouchOneByOne::create();
 
-        // add the sprite as a child to this layer
-        this->addChild(sprite, 0);
-    }
-    return true;
+	touchListener->onTouchBegan = CC_CALLBACK_2(HelloWorld::onTouchBegan, this);
+	touchListener->onTouchEnded = CC_CALLBACK_2(HelloWorld::onTouchEnded, this);
+	touchListener->onTouchMoved = CC_CALLBACK_2(HelloWorld::onTouchMoved, this);
+	touchListener->onTouchCancelled = CC_CALLBACK_2(HelloWorld::onTouchCancelled, this);
+
+	_eventDispatcher->addEventListenerWithSceneGraphPriority(touchListener, this);
+
+
+	return true;
 }
 
-
-void HelloWorld::menuCloseCallback(Ref* pSender)
+bool HelloWorld::onTouchBegan(Touch* touch, Event* event)
 {
-    //Close the cocos2d-x game scene and quit the application
-    Director::getInstance()->end();
+	cocos2d::log("touch began");
+	return true;
+}
 
-    /*To navigate back to native iOS screen(if present) without quitting the application  ,do not use Director::getInstance()->end() as given above,instead trigger a custom event created in RootViewController.mm as below*/
+void HelloWorld::onTouchEnded(Touch* touch, Event* event)
+{
+	_turn++;
 
-    //EventCustom customEndEvent("game_scene_close_event");
-    //_eventDispatcher->dispatchEvent(&customEndEvent);
+	int turn = _turn % 2;
+
+	auto sprite = turn == 1 ? Sprite::create("o_64.png") : Sprite::create("x_64.png");
 
 
+
+	sprite->setPosition(touch->getLocation());
+
+	this->addChild(sprite, _turn);
+
+	cocos2d::log("touch ended");
+}
+
+void HelloWorld::onTouchMoved(Touch* touch, Event* event)
+{
+	cocos2d::log("touch moved");
+}
+
+void HelloWorld::onTouchCancelled(Touch* touch, Event* event)
+{
+	cocos2d::log("touch cancelled");
 }
--- Classes/HelloWorldScene.h
+++ Classes/HelloWorldScene.h
@@ -1,18 +1,18 @@
 /****************************************************************************
  Copyright (c) 2017-2018 Xiamen Yaji Software Co., Ltd.
- 
+
  http://www.cocos2d-x.org
- 
+
  Permission is hereby granted, free of charge, to any person obtaining a copy
  of this software and associated documentation files (the "Software"), to deal
  in the Software without restriction, including without limitation the rights
  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  copies of the Software, and to permit persons to whom the Software is
  furnished to do so, subject to the following conditions:
- 
+
  The above copyright notice and this permission notice shall be included in
  all copies or substantial portions of the Software.
- 
+
  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -29,16 +29,26 @@
 
 class HelloWorld : public cocos2d::Scene
 {
-public:
-    static cocos2d::Scene* createScene();
+private:
+	int _turn;
 
-    virtual bool init();
-    
-    // a selector callback
-    void menuCloseCallback(cocos2d::Ref* pSender);
-    
-    // implement the "static create()" method manually
-    CREATE_FUNC(HelloWorld);
+	int _spriteSize = 64;
+	int _spriteMargin = 12;
+	int _originX = 0;
+	int _originY = 0;
+
+public:
+	static cocos2d::Scene* createScene();
+
+	virtual bool init();
+
+	virtual bool onTouchBegan(cocos2d::Touch*, cocos2d::Event*);
+	virtual void onTouchEnded(cocos2d::Touch*, cocos2d::Event*);
+	virtual void onTouchMoved(cocos2d::Touch*, cocos2d::Event*);
+	virtual void onTouchCancelled(cocos2d::Touch*, cocos2d::Event*);
+
+	// implement the "static create()" method manually
+	CREATE_FUNC(HelloWorld);
 };
 
 #endif // __HELLOWORLD_SCENE_H__
